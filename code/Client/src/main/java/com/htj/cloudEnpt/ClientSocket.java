cbdcc894e745c57ef26aa00ef0f0303e76cc405a
侯添久 <houtianjiu@bytedance.com>
2020-05-06 15:28:45 +0800

    客户端数据

--- code/Client/src/main/java/com/htj/cloudEnpt/ClientSocket.java
+++ code/Client/src/main/java/com/htj/cloudEnpt/ClientSocket.java
@@ -1,22 +1,28 @@
 package com.htj.cloudEnpt;
 
 
-import cn.xjfme.encrypt.utils.sm4.SM4Utils;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.LinkedList;
 
 public class ClientSocket {
 
+    private Socket socket = null; //单例客户端
+
+    /**
+     * 构造器初始化，主要进行ip和port的初始化
+     */
     public ClientSocket(){
         ServerProp.initIpAndPort("./src/main/resources/server.properties");
+        socket = Singleton.getClientSocket();
     }
 
-    public void getInput(){
+    /**
+     * 获取用户输入，然后加密，发送到服务端
+     */
+    public  void getInput() {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int i = 0;
+        int i = 1;
         String content = "";
         try{
             while (!"#".equals(content)){
@@ -24,37 +30,94 @@ public class ClientSocket {
                 content = in.readLine();
                 i++;
                 String encrptyText = encrptyData(content);
-                System.out.println("加密后的密文为：" + encrptyText);
                 sendData(encrptyText);
             }
+            receiveData();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public String decrptyData(String encryptData){
+    /**
+     * 加密函数，解密客户端从服务端收到的数据
+     * @param encryptData
+     * @return
+     */
+    public static String decrptyData(String encryptData){
         return Singleton.getSm4Utils().decryptData_ECB(encryptData);
     }
 
-    public String encrptyData(String plainText){
-        return Singleton.getSm4Utils().encryptData_ECB(plainText);
+    /**
+     * 加密函数，包含结尾符号处理
+     * @param plainText
+     * @return
+     */
+    public static String encrptyData(String plainText){
+        if ("#".equals(plainText)){
+            return "#";
+        }
+        String encrptyText =  Singleton.getSm4Utils().encryptData_ECB(plainText);
+        System.out.println("加密后的密文为：" + encrptyText);
+        return encrptyText;
     }
 
+    /**
+     * 通过socket发送数据
+     * @param msg
+     */
     public void sendData(String msg) {
-
         try {
-            Socket clientSocket = new Socket(ServerProp.ip, ServerProp.port);
-            OutputStream outputStream = clientSocket.getOutputStream();
+            //Socket socket = new Socket(ServerProp.ip, ServerProp.port); //每次传送数据一个新的tcp连接
+            OutputStream outputStream = socket.getOutputStream();
             outputStream.write(msg.getBytes("UTF-8")); //写出数据
-            clientSocket.shutdownOutput(); //数据发送完毕
+            outputStream.flush();
+            //socket.shutdownOutput();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        ClientSocket one = new ClientSocket();
-        one.getInput();
+    /**
+     * 接收服务端传递来的数据
+     */
+    public void receiveData(){
+        System.out.println("----------------------------------------");
+        System.out.println("开始接收服务端返回的数据");
+        try {
+            //Socket socket = new Socket(ServerProp.ip, ServerProp.port);
+            InputStream in = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            StringBuilder s = new StringBuilder();
+            int len = 0;
+            while ((len = in.read(buffer)) != -1) {
+               s.append(new String(buffer, 0, len, "UTF-8"));
+            }
+            showData(s.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * 对服务端的数据解密展示
+     */
+    public void showData(String s) {
+        String[] strs = s.split(",");
+        String encrptyText = strs[strs.length - 1];
+        System.out.println("总的解密的结果是：" + decrptyData(encrptyText));
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < strs.length - 1; i++){
+            res.append(decrptyData(strs[i]));
+        }
+        System.out.println("分开解密的结果是：" + res.toString());
+    }
+
+    /**
+     * 主函数，程序入口
+     * @param args
+     */
+    public static void main(String[] args) {
+       ClientSocket one = new ClientSocket();
+       one.getInput();
     }
 }
--- code/Client/src/main/java/com/htj/cloudEnpt/Singleton.java
+++ code/Client/src/main/java/com/htj/cloudEnpt/Singleton.java
@@ -2,8 +2,11 @@ package com.htj.cloudEnpt;
 
 import cn.xjfme.encrypt.utils.sm4.SM4Utils;
 
+import java.net.Socket;
+
 public class Singleton {
     private static volatile SM4Utils sm4Utils = null;
+    private static volatile Socket clientSocket = null;
 
     public static SM4Utils getSm4Utils(){
         if (sm4Utils == null){
@@ -15,5 +18,21 @@ public class Singleton {
             }
         }
         return sm4Utils;
+    }
+
+    public static Socket getClientSocket() {
+        if (clientSocket == null) {
+            synchronized (Singleton.class) {
+                if (clientSocket == null) {
+                    try {
+                        clientSocket = new Socket(ServerProp.ip, ServerProp.port);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                return clientSocket;
+            }
+        }
+        return clientSocket;
     }
 }
