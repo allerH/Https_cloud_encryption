72dff905ec04196b18e079ef2e7b5066f0e8be80
htj123 <1095833846@qq.com>
2020-06-11 16:58:14 +0800

    客户端修改

--- code/Client/src/main/java/com/htj/cloudEnpt/Singleton.java
+++ code/Client/src/main/java/com/htj/cloudEnpt/Singleton.java
@@ -10,7 +10,6 @@ public class Singleton {
     public static final String publicKey ="04BB34D657EE7E8490E66EF577E6B3CEA28B739511E787FB4F71B7F38F241D87F18A5A93DF74E90FF94F4EB907F271A36B295B851F971DA5418F4915E2C1A23D6E";
     public static final String privatekey = "0B1CE43098BC21B8E82B5C065EDB534CB86532B1900A49D49F3C53762D2997FA";
     private static volatile SM4Utils sm4Utils = null;
-    private static volatile SM3 sm3Utils = null;
     private static volatile Socket clientSocket = null;
 
     public static SM4Utils getSm4Utils(){
